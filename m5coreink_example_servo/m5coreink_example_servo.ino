8ae5086c1eb49400b88a007d1621efe13e264087
pa-nik <76602480+pa-nik@users.noreply.github.com>
2022-12-04 17:26:23 -0800

    modify servo example

--- m5coreink_example_servo/m5coreink_example_servo.ino
+++ m5coreink_example_servo/m5coreink_example_servo.ino
@@ -1,17 +1,17 @@
 // Example of controlling a servo using the scroll wheel on M5Core-Ink
 
 #include "M5CoreInk.h"          // M5Core-Ink library
-#include <Servo.h>              // Servo library
+#include <ESP32Servo.h>         // Servo library
 
 const int servoPin = 26;        // Servo pin
-Servo servoObj;                 // Servo object 
+Servo servoObj;                 // Servo object
 int servoVal = 0;               // Servo output value
 
 void setup() {
   M5.begin();
   Serial.begin(9600);
   Serial.println("M5Core-Ink Servo Example");
-  
+
   servoObj.attach(servoPin);    // attach Servo to servoPin
 }
 
@@ -28,6 +28,6 @@ void loop() {
     servoObj.write(servoVal);
     Serial.println("move servo to 180");
   }
-  
+
   M5.update();
 }
